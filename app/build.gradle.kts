2beaa20b78c9ed2f161588f1e583d319a5638f70
alexandrenavarro <alexandrenavarro@gmail.com>
2025-12-15 23:14:51 +0000

    chore: add lib retrofit to enable easy implementation of network api calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.material.icons)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

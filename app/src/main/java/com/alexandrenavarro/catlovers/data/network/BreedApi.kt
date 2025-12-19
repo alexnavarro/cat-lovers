5abc568ebd7acc21ae02466463c008f8f0c3453a
alexandrenavarro <alexandrenavarro@gmail.com>
2025-12-19 15:43:36 +0000

    feat: add new api call to fetch breed detail

--- app/src/main/java/com/alexandrenavarro/catlovers/data/network/BreedApi.kt
+++ app/src/main/java/com/alexandrenavarro/catlovers/data/network/BreedApi.kt
@@ -1,8 +1,10 @@
 package com.alexandrenavarro.catlovers.data.network
 
+import com.alexandrenavarro.catlovers.data.network.model.NetworkBreedDetail
 import com.alexandrenavarro.catlovers.data.network.model.NetworkBreedPreview
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BreedApi {
@@ -12,4 +14,9 @@ interface BreedApi {
         @Query("limit") limit: Int,
         @Query("page") page: Int
     ): Response<List<NetworkBreedPreview>>
+
+    @GET("/v1/breeds/{id}")
+    suspend fun fetchBreed(
+        @Path("id") id: String
+    ): Response<List<NetworkBreedDetail>>
 }

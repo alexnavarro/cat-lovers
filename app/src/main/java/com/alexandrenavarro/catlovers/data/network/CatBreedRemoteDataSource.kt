e3cd55fbd860576a50404605b99cadc3b04decf2
alexandrenavarro <alexandrenavarro@gmail.com>
2025-12-20 03:47:32 +0000

    remove unnecessary log

--- app/src/main/java/com/alexandrenavarro/catlovers/data/network/CatBreedRemoteDataSource.kt
+++ app/src/main/java/com/alexandrenavarro/catlovers/data/network/CatBreedRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.alexandrenavarro.catlovers.data.network
 
-import android.util.Log
 import com.alexandrenavarro.catlovers.data.network.model.NetworkCatBreedDetail
 import com.alexandrenavarro.catlovers.data.network.model.NetworkCatBreedPreview
 
@@ -23,7 +22,6 @@ internal class CatBreedRemoteDataSourceImpl(
         pageSize: Int
     ): Result<List<NetworkCatBreedPreview>> =
         safeApiCall {
-            Log.d("CatBreedRemoteDataSource", "Fetching cat breeds page $page")
             catBreedApi.fetchCatBreeds(limit = pageSize, page = page) }
 
     override suspend fun fetchCatBreed(id: String): Result<NetworkCatBreedDetail> =
